c5370c52c6f61f92d7b648f6516cefccb6b34198
Lukas Hoellein <lukas.hoellein@tum.de>
2020-04-14 13:29:02 +0200

    refactor into separate methods

--- data/nuim_dataloader.py
+++ data/nuim_dataloader.py
@@ -1,9 +1,7 @@
 import numpy as np
 import os
-import torch
 from torch.utils.data import Dataset
 from skimage import io
-
 
 class ICLNUIMDataset(Dataset):
     '''
@@ -29,31 +27,36 @@ class ICLNUIMDataset(Dataset):
         self.transform = transform
         self.depth_to_image_plane = depth_to_image_plane
         self.path = path
+        self.depth_cache = []
 
         self.img = sorted([f for f in os.listdir(path) if f.endswith('.png')])
         self.depth = sorted([f for f in os.listdir(path) if f.endswith('.depth')])
         self.cam = sorted([f for f in os.listdir(path) if f.endswith('.txt')])
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
 
-        # read image
-        image = io.imread(os.path.join(self.path, self.img[idx]))
+    def load_image(self, idx):
+        return io.imread(os.path.join(self.path, self.img[idx]))
 
-        # read depth file
+    def load_depth(self, idx, img_shape=(480,640)):
         with open(os.path.join(self.path, self.depth[idx])) as f:
-            depth = [float(i) for i in f.read().split(' ') if i.strip()] # read .depth file
-            depth = np.asarray(depth, dtype=np.float32).reshape(image.shape[:2]) # convert to same format as image WxH
+            depth = [float(i) for i in f.read().split(' ') if i.strip()]  # read .depth file
+            depth = np.asarray(depth, dtype=np.float32).reshape(img_shape)  # convert to same format as image WxH
 
             # convert to image plane depth by taking into account the position in the WxH array as (x, y)
             if self.depth_to_image_plane:
                 depth = np.fromfunction(lambda x, y: self.toImagePlane(depth, x, y), depth.shape, dtype=depth.dtype)
+        return depth
+
+    def __getitem__(self, idx):
+        image = self.load_image(idx)
+        depth = self.load_depth(idx)
 
         #TODO: read cam.txt file
 
-        sample = {'image': image,
-                  'depth': depth}
+        sample = {
+            'image': image,
+            'depth': depth
+        }
 
         if self.transform:
             sample['image'] = self.transform(sample['image'])
@@ -96,4 +99,7 @@ def test():
 
 if __name__ == "__main__":
     # execute only if run as a script
+
+    from joblib import Memory
+
     test()
