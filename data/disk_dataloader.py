a4fc4aa2f871acb0bd8f2aa83e885e2010e2d152
Lukas Hoellein <lukas.hoellein@tum.de>
2020-06-02 19:31:23 +0200

    Add baseclass dataloader + ICL/MP3D as subclass dataloaders

---
+++ data/disk_dataloader.py
@@ -0,0 +1,257 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+from util.camera_transformations import *
+import re
+import torchvision
+
+from abc import ABC, abstractmethod
+
+
+class ToNumpy(object):
+    def __call__(self, sample):
+        return np.array(sample)
+
+
+class ClipDepth(object):
+    '''Set maximal depth'''
+
+    def __init__(self, maxDepth):
+        self.maxDepth = maxDepth
+
+    def __call__(self, sample):
+        sample[sample>self.maxDepth] = self.maxDepth
+        return sample
+
+
+class DiskDataset(Dataset, ABC):
+    '''
+    Loads samples from files satisfying the following directory structure:
+    TODO describe
+    '''
+
+    # regex to read lines from the camera .txt file
+    cam_pattern = "(?P<id>.*\w).*= \[(?P<x>.*), (?P<y>.*), (?P<z>.*)\].*"
+
+    def __init__(self,
+                 path,
+                 maxDepth,
+                 imageInputShape,
+                 sampleOutput=True,
+                 inverse_depth=False,
+                 cacheItems=False,
+                 transform=None):
+        '''
+
+        :param path: path/to/<base>/files. Needs to be a directory with .png, .depth and .txt files
+        :param maxDepth: maximum depth that is accepted. Everything above that will be cut to given value.
+        :param imageInputShape: the original image input shape for the dataset that gets used. Necessary to reshape depth values into correct array dimensions.
+        :param sampleOutput: whether or not to uniformly sample a second image + extrinsic camera pose (R|T) in the neighborhood of each accessed item.
+                neighborhood is currently defined as: select uniformly at random any camera in the range [index-30, index+30) where index is the accessed item index.
+                For example: If the 500. item is accessed, the second camera pose (R|T) will be from any of the poses of the items 470-530 (excluding 500).
+        :param inverse_depth: If true, depth.pow(-1) is returned for the depth file (changing depth BEFORE applying transform object).
+        :param cacheItems: If true, all items will be stored in RAM dictionary, once they were accessed.
+        :param transform: transform that should be applied to the input image AND the target depth
+        '''
+        self.transform = transform
+        self.K, self.Kinv = self.load_int_cam()
+        self.imageInputShape = imageInputShape
+
+        # Fix for this issue: https://github.com/pytorch/vision/issues/2194
+        if isinstance(self.transform.transforms[-1], torchvision.transforms.ToTensor):
+            self.transform_depth = torchvision.transforms.Compose([
+                *self.transform.transforms[:-1],
+                ToNumpy(),
+                ClipDepth(maxDepth),
+                torchvision.transforms.ToTensor()
+            ])
+        else:
+            self.transform_depth = self.transform
+
+        self.inverse_depth = inverse_depth
+        self.maxDepth = maxDepth
+
+        self.path = path
+        self.img = sorted([f for f in os.listdir(path) if f.endswith('.png')])
+        self.depth = sorted([f for f in os.listdir(path) if f.endswith('.depth')])
+        self.depth_binary = sorted([f for f in os.listdir(path) if f.endswith('.depth.npy')])
+        self.has_binary_depth = len(self.depth_binary) > 0
+        self.cam = sorted([f for f in os.listdir(path) if f.endswith('.txt')])
+
+        self.size = len(self.img)
+
+        self.sampleOutput = sampleOutput
+        if self.sampleOutput:
+            self.inputToOutputIndex = []
+            for idx in range(self.size):
+                # sample second idx in [idx-30, idx+30) interval
+                low = idx - 30 if idx >= 30 else 0
+                high = idx + 30 if idx <= self.size - 30 else self.size
+                output_idx = np.random.randint(low, high, 1)[0] # high is exclusive
+
+                # never return the same idx, default handling: just use +1 or -1 idx
+                if output_idx == idx and self.size > 1: # if we only have one sample, we can do nothing about this.
+                    output_idx = idx+1 if idx < self.size-1 else idx-1
+
+                self.inputToOutputIndex.append(output_idx)
+
+        self.cacheItems = cacheItems
+        self.itemCache = [None for i in range(self.size)]
+
+    def load_image(self, idx):
+        return Image.open(os.path.join(self.path, self.img[idx]))
+
+    def load_depth(self, idx):
+        if self.has_binary_depth:
+            # read faster from .depth.npy file - this is much faster than parsing the char-based .depth file from ICL directly.
+            depth = np.load(os.path.join(self.path, self.depth_binary[idx]))
+        else:
+            with open(os.path.join(self.path, self.depth[idx])) as f:
+                depth = [float(i) for i in f.read().split(' ') if i.strip()]  # read .depth file
+                depth = np.asarray(depth, dtype=np.float32).reshape(self.imageInputShape)  # convert to same format as image HxW
+
+        # Implementation specific
+        depth = self.modify_depth(depth)
+
+        # invert depth
+        if self.inverse_depth:
+            depth = np.power(depth, -1)
+
+        return Image.fromarray(depth, mode='F') # return as float PIL Image
+
+    def load_ext_cam(self, idx):
+        cam = {} # load the .txt file in this dict
+        with open(os.path.join(self.path, self.cam[idx])) as f:
+            for line in f:
+                m = re.match(DiskDataset.cam_pattern, line) # will match everything except angle, but that is not needed anyway
+                if m is not None:
+                    cam[m["id"]] = np.zeros(3)
+                    cam[m["id"]][0] = float(m["x"])
+                    cam[m["id"]][1] = float(m["y"])
+                    cam[m["id"]][2] = float(m["z"])
+
+        # calculate RT matrix, taken from: https://www.doc.ic.ac.uk/~ahanda/VaFRIC/computeRT.m
+        z = cam["cam_dir"] / np.linalg.norm(cam["cam_dir"])
+        x = np.cross(cam["cam_up"], z)
+        x = x / np.linalg.norm(x)
+        y = np.cross(z, x)
+
+        # combine in correct shape
+        RT = np.column_stack((x, y, z, cam["cam_pos"]))
+        RT = np.vstack([RT, [0,0,0,1]])
+        RT = RT.astype(np.float32)
+
+        # calculate RTinv from RT
+        RTinv = np.linalg.inv(RT).astype(np.float32)
+
+        # return as torch tensor
+        RT = torch.from_numpy(RT)
+        RTinv = torch.from_numpy(RTinv)
+
+        return RT, RTinv
+
+    @abstractmethod
+    def load_int_cam(self):
+        pass
+
+    @abstractmethod
+    def modify_depth(self, depth):
+        pass
+
+    def __getitem__(self, idx):
+        """
+
+        :param idx: item to choose
+        :return: dictionary with following format:
+            {
+                'image': image,
+                'depth': depth,
+                'cam': cam,
+                'output': output
+            }
+            where
+              image is a WxHxC matrix of floats,
+              depth is a WxH matrix of floats,
+              cam is a dictionary:
+                {
+                    'RT1': RT1,
+                    'RT1inv': RT1inv,
+                    'RT2': RT2,
+                    'RT2inv': RT2inv,
+                    'K': ICLNUIMDataset.K,
+                    'Kinv': ICLNUIMDataset.Kinv
+                }
+                where
+                  RT1 is a 4x4 extrinsic matrix of the idx-th item,
+                  RT2 is a 4x4 extrinsic matrix of a random neighboring item or None (see self.sampleOutput)
+                  K is a 4x4 intrinsic matrix (constant over all items) with 4th row/col added for convenience,
+                  *inv is the inverted matrix
+              output is a dictionary or None (see self.sampleOutput):
+                {
+                  'image': output_image,
+                  'idx': output_idx
+                }
+                where
+                  image is a random neighboring image
+                  idx is the index of the neighboring image (and of cam['RT2'])
+
+
+
+        """
+
+        if self.itemCache[idx] is not None:
+            return self.itemCache[idx]
+
+        image = self.load_image(idx)
+        depth = self.load_depth(idx)
+        RT1, RT1inv = self.load_ext_cam(idx)
+
+        cam = {
+            'RT1': RT1,
+            'RT1inv': RT1inv,
+            'K': self.K,
+            'Kinv': self.Kinv
+        }
+
+        output = None
+        if self.sampleOutput:
+            # lookup output for this sample
+            output_idx = self.inputToOutputIndex[idx]
+
+            # load image of new index
+            output_image = self.load_image(output_idx)
+            output_depth = self.load_depth(output_idx)
+
+            # load cam of new index
+            RT2, RT2inv = self.load_ext_cam(output_idx)
+
+            cam['RT2'] = RT2
+            cam['RT2inv'] = RT2inv
+
+            output = {
+                'image': output_image,
+                'depth': output_depth,
+                'idx': output_idx
+            }
+
+        sample = {
+            'image': image,
+            'depth': depth,
+            'cam': cam,
+            'output': output
+        }
+
+        if self.transform:
+            sample['image'] = self.transform(sample['image'])
+            sample['depth'] = self.transform_depth(sample['depth'])
+            if self.sampleOutput:
+                sample['output']['image'] = self.transform(sample['output']['image'])
+                sample['output']['depth'] = self.transform_depth(sample['output']['depth'])
+
+        if self.cacheItems:
+            self.itemCache[idx] = sample
+
+        return sample
+
+    def __len__(self):
+        return self.size
--- data/mp3d_dataloader.py
+++ data/mp3d_dataloader.py
@@ -4,18 +4,17 @@ import torchvision
 from data.disk_dataloader import DiskDataset
 
 
-class ICLNUIMDataset(DiskDataset):
+class MP3D_Habitat_Offline_Dataset(DiskDataset):
     '''
-    Loads samples from the pre-rendered NUIM dataset: https://www.doc.ic.ac.uk/~ahanda/VaFRIC/iclnuim.html
-    Adapted from c++ code in https://www.doc.ic.ac.uk/~ahanda/VaFRIC/codes.html
+    Loads samples from the Matterport3D dataset via Habitat from disk.
     '''
 
-    # intrinsic camera matrix taken from https://www.doc.ic.ac.uk/~ahanda/VaFRIC/codes.html
+    # intrinsic camera matrix
     cam_K = {
-        'fx': 481.2,
-        'fy': -480.0,
-        'cx': 319.5,
-        'cy': 239.5
+        'fx': 1.0,
+        'fy': 1.0,
+        'cx': 0.0,
+        'cy': 0.0
     }
 
     # intrinsic camera matrix as torch tensor
@@ -32,19 +31,16 @@ class ICLNUIMDataset(DiskDataset):
     Kinv[:3,:3] = invert_K(K[:3,:3])
     Kinv[3,3] = 1
 
-    # max depth that we accept for ICL - can be bigger but we cut it at this value
+    # max depth that we accept for MP3D - can be bigger but we cut it at this value
     max_depth = 10.0
-
-    # shape from ICL images in its original resolution. Format: HxW
-    image_shape = (480, 640)
 
     def __init__(self,
                  path,
+                 in_size,
                  sampleOutput=True,
                  inverse_depth=False,
                  cacheItems=False,
-                 transform=None,
-                 out_shape=(480,640)):
+                 transform=None):
         '''
 
         :param path: path/to/NUIM/files. Needs to be a directory with .png, .depth and .txt files, as can be obtained from: https://www.doc.ic.ac.uk/~ahanda/VaFRIC/iclnuim.html
@@ -53,53 +49,22 @@ class ICLNUIMDataset(DiskDataset):
                 For example: If the 500. item is accessed, the second camera pose (R|T) will be from any of the poses of the items 470-530 (excluding 500).
         :param inverse_depth: If true, depth.pow(-1) is returned for the depth file (changing depth BEFORE applying transform object).
         :param transform: transform that should be applied to the input image AND the target depth
-        :param out_shape: the output shape of images. If you apply transform that changes the output shape, we need to reflect this in a modified K matrix. Thus,
-                if a "Resize" can be found in transform, then K will be recalculated w.r.t. the out_shape provided here. TODO: handle more than just "Resize".
+        :param in_size: size of rectangular images read from disk
         '''
-        self.out_shape = out_shape
-
         DiskDataset.__init__(self,
                              path=path,
-                             maxDepth=ICLNUIMDataset.max_depth,
-                             imageInputShape=ICLNUIMDataset.image_shape,
+                             maxDepth=MP3D_Habitat_Offline_Dataset.max_depth,
+                             imageInputShape=(in_size, in_size),
                              sampleOutput=sampleOutput,
                              inverse_depth=inverse_depth,
                              cacheItems=cacheItems,
                              transform=transform)
 
     def modify_depth(self, depth):
-        return np.fromfunction(lambda y, x: self.toImagePlane(depth, x, y), depth.shape, dtype=depth.dtype)
+        return depth # nothing to do here
 
     def load_int_cam(self):
-        if self.transform is not None and 'Resize' in str(self.transform):
-            K2 = torch.from_numpy(np.zeros((4,4)).astype(np.float32))
-            K2[0,0] = 0.751875 * self.out_shape[1]      #cam_K2['fx']
-            K2[1,1] = -1.0 * self.out_shape[0]          #cam_K2['fy']
-            K2[0,2] = 0.5 * self.out_shape[1]           #cam_K2['cx']
-            K2[1,2] = 0.5 * self.out_shape[0]           #cam_K2['cy']
-            K2[2,2] = 1
-            K2[3,3] = 1 # we use 4x4 matrix for easier backward-calculations without removing indices, see projection/z_buffer_manipulator.py
-
-            K2inv = torch.from_numpy(np.zeros((4,4)).astype(np.float32))
-            K2inv[:3,:3] = invert_K(K2[:3,:3])
-            K2inv[3,3] = 1
-
-            return K2, K2inv
-        else:
-            return ICLNUIMDataset.K, ICLNUIMDataset.Kinv
-
-    def toImagePlane(self, depth, x, y):
-
-        # taken from the figure in: https://www.doc.ic.ac.uk/~ahanda/VaFRIC/codes.html
-        #z = ICLNUIMDataset.cam_K['fx'] * np.sqrt( (depth**2) / (x**2 + y**2 + ICLNUIMDataset.cam_K['fx']**2))
-
-
-        # taken from the c++ code implementation at https://www.doc.ic.ac.uk/~ahanda/VaFRIC/codes.html in file VaFRIC.cpp#getEuclidean2PlanarDepth
-        x_plane = (x - ICLNUIMDataset.cam_K['cx']) / ICLNUIMDataset.cam_K['fx']
-        y_plane = (y - ICLNUIMDataset.cam_K['cy']) / ICLNUIMDataset.cam_K['fy']
-        z = depth / np.sqrt(x_plane ** 2 + y_plane ** 2 + 1)
-
-        return z
+        return MP3D_Habitat_Offline_Dataset.K, MP3D_Habitat_Offline_Dataset.Kinv
 
 
 def getEulerAngles(R):
@@ -112,21 +77,21 @@ def getEulerAngles(R):
 
 def test():
     transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((256, 256)),
+        torchvision.transforms.Resize((512, 512)),
         torchvision.transforms.ToTensor(),
     ])
 
-    dataset = ICLNUIMDataset("/home/lukas/Desktop/datasets/ICL-NUIM/prerendered_data/living_room_traj2_loop",
+    dataset = MP3D_Habitat_Offline_Dataset("/home/lukas/Desktop/git/synsin/dataset",
+                             in_size=256,
                              sampleOutput=True,
                              inverse_depth=False,
                              cacheItems=False,
                              transform=transform)
-    #dataset = ICLNUIMDataset("sample", sampleOutput=True, transform=transform);
 
     print("Length of dataset: {}".format(len(dataset)))
 
     # Show first item in the dataset
-    i = 400
+    i = 0
     item = dataset.__getitem__(i)
 
     #print(item["depth"].numpy().flags)
