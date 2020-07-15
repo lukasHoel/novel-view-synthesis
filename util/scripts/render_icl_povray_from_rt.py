b165c48ab4933368d38edb5a63b4938dd474eb4c
Lukas Hoellein <lukas.hoellein@tum.de>
2020-07-15 18:53:00 +0200

    Add movement of mesh to rendering script

--- util/scripts/render_icl_povray_from_rt.py
+++ util/scripts/render_icl_povray_from_rt.py
@@ -1,14 +1,117 @@
 import sys
 import subprocess
+import json
 
-from data.nuim_dataloader import ICLNUIMDataset
+import numpy as np
+
+from tempfile import mkstemp
+from shutil import move, copymode
+from os import fdopen, remove, path
+
+from data.nuim_dynamics_dataloader import ICLNUIM_Dynamic_Dataset
+
+
+def modify_pov_mesh(povray_icl_folder, icl_dataset):
+    # load dynamics
+    with open(sys.argv[3]) as f:
+        dynamics = json.load(f)
+        dynamics = dynamics[0]  # TODO SUPPORT MULTIPLE TRANSFORMATIONS IN ONE JSON
+
+        dynamics["name"] = dynamics["name"][:len(dynamics["name"]) // 2] # in moved.txt the name is contained twice with a separating "_" in between, but in the mesh file it is not contained twice
+        print(dynamics["name"])
+
+        transformation = dynamics["transformation"]
+        transformation = np.asarray(transformation).reshape((3, 4))
+        transformation = icl_dataset.modify_dynamics_transformation(transformation)
+
+        print(transformation)
+
+    # Create temp file
+    fh, abs_path = mkstemp()
+
+    # construct path to original mesh file
+    orig_mesh_file = path.join(povray_icl_folder, "living_room_POV_geom_original.inc")
+
+    # construct path to actually used mesh file (the povray render script loads this file name)
+    used_mesh_file = path.join(povray_icl_folder, "living_room_POV_geom.inc")
+
+    # tells if we have found the place where to replace vertices in the file
+    found = False
+    skip_writing_range = None
+    modified_vertex_lines = []
+
+    with fdopen(fh, 'w') as new_file:
+        with open(orig_mesh_file) as old_file:
+            lines = old_file.readlines()
+            for idx, line in enumerate(lines):
+                if dynamics["name"] in line and not found:
+
+                    print(f"Found starting point at line {idx}: {line}")
+
+                    found = True
+                    number_vertices = int(lines[idx+2][:-2]) # second next line is always in format '<vertices>,'
+                    skip_writing_range = range(idx+3, idx+3+number_vertices) # these lines are all lines of the vertices in format '<x,y,z>'
+
+                    print(f"Need to modify {number_vertices} vertices in line-range {skip_writing_range}")
+
+                    # create the new vertex lines
+                    for vertex_idx, vertex_line in enumerate(lines[skip_writing_range[0]:skip_writing_range[-1] + 1]):
+                        print()
+                        print(f"Convert {vertex_idx}-th line {vertex_line}")
+
+                        if vertex_idx == len(skip_writing_range) - 1:
+                            vertex_line = vertex_line[1:-2]  # remove leading '<' and trailing '>'
+                        else:
+                            vertex_line = vertex_line[1:-3] # remove leading '<' and trailing '>,'
+
+                        print(f"Stripped line to {vertex_line}")
+
+                        # parse vertex into numpy array
+                        vertex = np.array([float(x) for x in vertex_line.split(",")])
+                        vertex = np.append(vertex, 1) # add homogenous coordinate dimension
+
+                        print(f"Converted line to vector {vertex}")
+
+                        # transform vertex
+                        vertex = transformation @ vertex
+
+                        print(f"Transformed line to {vertex}")
+
+                        # write modified line
+                        if vertex_idx == len(skip_writing_range) - 1:
+                            vertex_line = f'<{vertex[0]},{vertex[1]},{vertex[2]}>\n' # add leading '<' and trailing '>'
+                        else:
+                            vertex_line = f'<{vertex[0]},{vertex[1]},{vertex[2]}>,\n' # add leading '<' and trailing '>,'
+
+                        modified_vertex_lines.append(vertex_line)
+                        print(f"Modified line to {vertex_line}")
+
+                # write this line: either from modified vertices or copy line
+                if found and idx in skip_writing_range:
+                    line = modified_vertex_lines[idx - skip_writing_range[0]]
+                    print(f"Write modified vertex {line}")
+                    new_file.write(line)
+                else:
+                    new_file.write(line)
+
+    # Copy the file permissions from the old file to the new file
+    copymode(used_mesh_file, abs_path)
+
+    # Remove used mesh file
+    remove(used_mesh_file)
+
+    # Move new file to used mesh file
+    move(abs_path, used_mesh_file)
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 3:
-        raise ValueError('Usage: ' + sys.argv[0] + ' <path_to_icl_with_camera_angles> <path_to_povray_icl_folder>')
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        raise ValueError('Usage: ' + sys.argv[0] + ' <path_to_icl_with_camera_angles> <path_to_povray_icl_folder> (<path_to_moved.txt>)')
 
-    d = ICLNUIMDataset(sys.argv[1])
+    d = ICLNUIM_Dynamic_Dataset(sys.argv[1])
+
+    if len(sys.argv) == 4:
+        modify_pov_mesh(sys.argv[2], d)
 
     for i in range(d.__len__()):
         item = d.__getitem__(i)
